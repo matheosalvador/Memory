f74501c37b30ad3f2ca31d5ccff0592758c9f7b9
lucas.V <78167286+LucasVe95@users.noreply.github.com>
2025-10-16 12:55:22 +0200

    Update sql_projet_flash_1_a_14.sql
    Création  de la base de donnée avec la table main_user et game

--- SQL/sql_projet_flash_1_a_14.sql
+++ SQL/sql_projet_flash_1_a_14.sql
@@ -1,1 +1,28 @@
+CREATE DATABASE projet_flash CHARACTER SET 'utf8';
 
+
+
+-- user story 1 et 3
+-- création de la table de l'utilisateur
+CREATE TABLE main_user (
+    id INT UNSIGNED  AUTO_INCREMENT,
+    email VARCHAR(256) NOT NULL UNIQUE, -- textarea de lemail avec 256 caractère max
+    mdp VARCHAR(256) NOT NULL UNIQUE, -- textarea du mot de passe avec 256 caractère max
+    pseudo VARCHAR(256) NOT NULL UNIQUE, -- textarea du pseudo avec 256 caractère max
+    created_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- inscription 
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- derniere connexion
+    PRIMARY KEY (id)
+)
+CHARACTER SET 'utf8'
+ENGINE = INNODB;
+
+
+
+-- création de la table du jeu
+CREATE TABLE game (
+    id INT UNSIGNED AUTO_INCREMENT,
+    game_name TEXT NOT NULL,
+    PRIMARY KEY (id)
+)
+CHARACTER SET 'utf8'
+ENGINE = INNODB;
