c466a3b0ab8cec9d8369dab65cc1d42cf0781138
matheosalvador <matheo.salvador24@gmail.com>
2025-11-06 11:40:03 +0100

    Myaccount début Co-authored-by: Thom <ZRThom@users.noreply.github.com>

--- myAccount.php
+++ myAccount.php
@@ -1,1 +1,30 @@
-<!-- Page de gestion du compte -->
+<?php require('utils/helper.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en"> <!-- language define to english-->
+    <head>
+        <meta charset="utf-8"> <!-- every character set here-->
+        <link rel="stylesheet" href="<?= getBaseUrl(); ?>\assets\css\myaccount.css">
+        <link rel="stylesheet" href="<?= getBaseUrl(); ?>\assets\css\main.css">
+        <title>My Account</title>
+    </head>
+
+    <?php include 'partials\header-terminé.php' ?>
+    <body>
+        <div>
+            <div class="flex-acc">
+                <img class="img" src="assets\images\Account-Logo.png" alt="logoaccount">
+            </div>
+            </div>
+            <div>
+                <span class="title-acc">Profil</span>
+            </div>
+            </div>
+            <hr class="line-acc">
+            <div class="w-5">
+                <h1>Hello</h1>    
+            </div>
+        </div>    
+    </body>    
+    <?php include 'partials\footer-terminé.php' ?>
+</html>
